6b7eace65da73e5695d7ac2b3002526029abd13f
tomarrajesh <rajeshtomar.delhi@gmail.com>
2016-09-12 10:44:52 +0530

    configuring aop support

--- src/main/java/com/webservice/ticketingservice/configuration/TicketingServiceConfiguration.java
+++ src/main/java/com/webservice/ticketingservice/configuration/TicketingServiceConfiguration.java
@@ -2,11 +2,12 @@ package com.webservice.ticketingservice.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-//@EnableAspectJAutoProxy
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 @ComponentScan(basePackages = "com.webservice.ticketingservice")
 public class TicketingServiceConfiguration {
 
